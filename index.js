f43e734bfe845cf487cf48ac3d00adaa08125836
Oluwafunmilayo Adewodu <funmiadewodu@yahoo.com>
2017-08-13 12:52:36 +0100

    updating index.js

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 $(document).ready(function() {
 
   function loadMoreContent() {
-    setTimeout(showTweets, 1000);
+    setTimeout(loadMore, 1000);
+    // loadMore();
   }
 
   function showTweets() {
     document.getElementById("loader").style.display = "none";
-    document.getElementById("myDiv").style.display = "block";
+    // document.getElementById("myDiv").style.display = "block";
+  }
+
+  var listElm = $('#infiniteList');
+  var nextTweet = " Kickstarter nostrud quis aesthetic butcher stuff. Echo park helvetica heirloom mumblecore enim kogi"
+  function loadMore () {
+
+    for (var i = 0; i < 20; i++) {
+      var item = document.createElement('div');
+      item.className = 'new-tweet animate-bottom'
+      item.innerHTML = nextTweet;
+      document.getElementById("loader").style.display = "none";
+      listElm.append(item);
+    }
   }
 
   $('#infiniteList').on('scroll', function() {
-  if ($('#infiniteList').scrollTop + $('#infiniteList').height >= $('#infiniteList').height) {
+    if ($('#infiniteList').scrollTop + $('#infiniteList').height >= $('#infiniteList').height) {
     loadMoreContent();
-  }
-});
+    }
+  });
 
 
 });
